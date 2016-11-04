82c762e41e08265678acd9432ca3a42a958c512f
Your Name <you@example.com>
2016-11-04 18:18:16 +0530

    pushed DES

---
+++ DES/des.h
@@ -0,0 +1,20 @@
+#ifndef _DES_H_
+#define _DES_H_
+
+// Systems args flags -e 1 , -d 0 ; Encryption and Decryption
+#define ENCRYPTION_MODE 1
+#define DECRYPTION_MODE 0
+
+typedef struct {
+	unsigned char k[8];
+	unsigned char c[4];
+	unsigned char d[4];
+} key_set;
+
+// Required functions for symmetric key crypto systems
+
+void generate_key(unsigned char* key);
+void generate_sub_keys(unsigned char* main_key, key_set* key_sets);
+void process_message(unsigned char* message_piece, unsigned char* processed_piece, key_set* key_sets, int mode);
+
+#endif
